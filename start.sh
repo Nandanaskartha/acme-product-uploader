ce6fdcf394f927ae12de8c573400073583664b66
Nandana <nandana.sreekumar20@gmail.com>
2025-11-16 12:23:02 +0530

    Run celery worker along with fast-api

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-celery -A app.celery_app worker --loglevel=info --concurrency=2 &
+#!/bin/bash
 
+# Start Celery worker in background with limited concurrency
+celery -A app.celery_app worker --loglevel=info --concurrency=1 &
+
+# Give celery a moment to start
+sleep 2
+
+# Start FastAPI with uvicorn
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
